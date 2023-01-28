9f90db5a21b08642fe465f07c063544ff93a66ee
Nick Shockey <shockeynj@gmail.com>
2023-01-27 20:49:28 -0500

    Basic routing

---
+++ src/routes/Other.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "Other",
+});
+</script>
+
+<template>
+	<p>Other page</p>
+</template>
---
+++ src/routes/router.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./Home.vue";
+import Other from "./Other.vue";
+
+const routes = [
+	{ path: "/", name: "Home", component: Home },
+	{ path: "/other", name: "Other", component: Other },
+];
+
+// const process =
+
+const router = createRouter({
+	history: createWebHistory("/"),
+	routes,
+});
+
+export default router;
